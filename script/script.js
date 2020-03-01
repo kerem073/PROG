5407b54f3d258028b4ea513a5ccb843918a1ae96
Kerem Kaya <keremkaya@hotmail.nl>
2020-03-01 17:24:18 +0100

    Titel aangepast en scroll appearing geprobeerd

--- script/script.js
+++ script/script.js
@@ -2,10 +2,14 @@ $(function(){
     // gsap.from(".titel", {duration:3, opacity:0, ease:"ease-out"});
     rotateTitle();
     levitate();
-    $("body").mousemove(mouseMove);
+    $(document).mousemove(mouseMove);
+    scrolled();
 });
 
 const $titel = $('.zweven-titel');
+const $flying_dirt = $('#groot_grasblok1');
+
+gsap.to($flying_dirt,{opacity:0});
 
 function rotateTitle(){
     $('.zweven-titel').each(function(){
@@ -18,15 +22,24 @@ function rotateTitle(){
 
 function levitate(){
     $(".zweven-titel").each(function(){
-        gsap.to(this, {duration:2, y: Math.random()*40 + 20, ease: "back.inOut(1.7)", yoyoEase:true, repeat:-1});
-        gsap.to(this, {duration:.8, rotation: Math.random()*10 - 20 + 10, ease:"power2.inOut", yoyoEase:true, repeat:-1, transformOrigin:(Math.random()*30 - 60) + "%"});
+        // gsap.to(this, {duration:2, y: Math.random()*40 + 20, ease: "back.inOut(1.7)", yoyoEase:true, repeat:-1});
+        gsap.to(this, {duration:2, rotation: Math.random()*10 - 20 + 10, ease:"power2.inOut", yoyoEase:true, repeat:-1, transformOrigin:(Math.random()*30 - 60) + "%"});
     })
 }
 
 function mouseMove(evt){
-    let translatex = evt.pageX / 60;
-    let translatey = evt.pageY / 60;
+    let translatex = (evt.clientX / $(window).width()) - .5;
+    let translatey = (evt.clientY / $(window).height()) - .5;
 
 
-    TweenMax.to($titel, 0.2, {x:translatex, y:translatey})
+    TweenLite.to($titel, 0.2, {x:translatex * 30, y:translatey * 30});
+}
+
+function scrolled(){
+    // Wanneer je scrolled moet de grootte grasblok  tevoorschijn komen
+    if($('#groot_grasblok1').scrollBottom < 10){
+        gsap.to($('#groot_grasblok1'), {duration:1, x:40, opacity:1});
+        
+    }
+    console.log($('#groot_grasblok1') + " : " + $('#groot_grasblok1').scrollBottom);
 }
